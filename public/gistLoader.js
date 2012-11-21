877903e0b041663f9def18b9e7a0105097b20d15
Patrick Huber <stackmagic@gmail.com>
2012-11-21 21:19:42 +0100

    add jq

--- public/gistLoader.js
+++ public/gistLoader.js
@@ -1,15 +1,24 @@
 (function() {
-    
+
+    var jqueryPath      = "https://raw.github.com/stackmagic/gist-Blogger/master/public/jquery-1.8.3.min.js";
     var gistBloggerPath = "https://raw.github.com/moski/gist-Blogger/master/public/gistBlogger.js";
 
     // Initialize the load the main script.
     try {
         initGist();
     } catch(e) {
-        var scriptId = 'gistPrinter';
-        if (document.getElementById(scriptId) === null) {
+        var jqueryScriptId = "jquery";
+        if (document.getElementById(jqueryScriptId) == null) {
             var elem = document.createElement('SCRIPT');
-            elem.id = scriptId;
+            elem.id = jqueryScriptId;
+            var theBody = document.getElementsByTagName('body')[0];
+            theBody.appendChild(elem);
+        }
+
+        var gistScriptId = 'gistPrinter';
+        if (document.getElementById(gistScriptId) === null) {
+            var elem = document.createElement('SCRIPT');
+            elem.id = gistScriptId;
             elem.onload = function() {
               initGist();
             }
