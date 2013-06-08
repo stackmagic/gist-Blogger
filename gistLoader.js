26e2bb7c24e1fed8a0443b944aa3c944c3f563f0
Patrick Huber <stackmagic@gmail.com>
2013-06-08 16:06:55 +0200

    new urls

--- gistLoader.js
+++ gistLoader.js
@@ -6,7 +6,7 @@ function loadLoader() {
         elem.onload = function() {
           initGist();
         }
-        elem.src = "https://raw.github.com/stackmagic/gist-Blogger/master/public/gistBlogger.js";
+        elem.src = "http://stackmagic.github.io/gistblogger/gistBlogger.js";
         var theBody = document.getElementsByTagName('body')[0];
         theBody.appendChild(elem);
     }
@@ -23,7 +23,7 @@ function loadLoader() {
             elem.onload = function() {
                 loadLoader();
             }
-            elem.src = "https://raw.github.com/stackmagic/gist-Blogger/master/public/jquery-1.8.3.min.js";
+            elem.src = "http://stackmagic.github.io/gistblogger/jquery-1.8.3.min.js";
             var theBody = document.getElementsByTagName('body')[0];
             theBody.appendChild(elem);
         }
